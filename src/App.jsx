e71ef1b4e5cff64f622c7cc2d595a74b0c55f798
devLuisDiaz <lualdz@gmail.com>
2023-08-27 19:53:50 -0400

    Cree un cuadro arriba azul y aun no tiene botones

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import UilReact from "@iconscout/react-unicons/icons/uil-react";
+import TopButtons from "./components/TopButtons";
 
 function App() {
-
-
   return (
-    <>
-    <h1 className='text-3xl font-bold underline'>Hola</h1>
-    </>
-  )
+    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
+      <TopButtons />
+    </div>
+  );
 }
 
-export default App
+export default App;
